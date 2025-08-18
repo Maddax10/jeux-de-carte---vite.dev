defb3b79e3e00497ff887e93332e53d94c76b131
Max <maximilien01993@gmail.com>
2025-08-18 13:07:13 +0200

    Refactor Card class and update main script for card generation logic

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,8 +1,12 @@
 export class Card {
   constructor(value){
     this.value = value;
+    this.played = false;
   }
   getValue = () => {
     return this.value;
+  }
+  setPlayedToTrue = () => {
+    this.played = true;
   }
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,52 @@
 import '/src/styles/main.scss'
 import { Card } from "./card.js"
 let allCards = new Array();
-const emptyCard =  new Card(0);
+const emptyCard = new Card(0);
 
-let myCurrentCard = emptyCard;
+//Carte qu'on doit trouver
+let CurrentCard = emptyCard;
+let myCards = new Array()
 
-//___________________________________________________________
-//Générer toutes les cartes possibles
-//___________________________________________________________
+//———————————————————————————————————————————————————————————
+// génération des cartes
+//———————————————————————————————————————————————————————————
 
+//Génération de toutes les cartes
 const generateAllCards = (nbCards) => {
-  for (let i = 1; i < nbCards+1; i++) {
+  for (let i = 1; i < nbCards + 1; i++) {
     allCards.push(new Card(i));
   }
 }
+
 generateAllCards(10);
-//-----------------
-//Fin génération de toutes les cartes
+
+//Attribution des cartes dans notre main
+const getFiveCards = () => {
+  let rnd;
+  let usedRnd = new Array();
+  let i = 0;
+  while (i < 5) {
+    rnd = Math.floor(Math.random() * 10);
+    if (!usedRnd.includes(rnd)) {
+      myCards.push(allCards[rnd]);
+      usedRnd.push(rnd);
+      i++;
+    }
+  }
+}
+
+getFiveCards();
+console.log("myCards", myCards);
+
+//-----------------------------------------------------------
+//Fin génération
+//-----------------------------------------------------------
+
+//———————————————————————————————————————————————————————————
+// Manipulation des cards
+//———————————————————————————————————————————————————————————
+
+
+//-----------------------------------------------------------
+//Fin manipulation
+//-----------------------------------------------------------
