733309c8e956576a5991c02da937ee1ec990e75e
Max <maximilien01993@gmail.com>
2025-08-18 13:12:42 +0200

    Refactor random card selection logic and add sorting functionality for hand

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,10 @@ const emptyCard = new Card(0);
 let CurrentCard = emptyCard;
 let myCards = new Array()
 
+const getRandom = () => {
+  return Math.floor(Math.random() * 10);
+}
+
 //———————————————————————————————————————————————————————————
 // génération des cartes
 //———————————————————————————————————————————————————————————
@@ -26,7 +30,7 @@ const getFiveCards = () => {
   let usedRnd = new Array();
   let i = 0;
   while (i < 5) {
-    rnd = Math.floor(Math.random() * 10);
+    rnd = getRandom();
     if (!usedRnd.includes(rnd)) {
       myCards.push(allCards[rnd]);
       usedRnd.push(rnd);
@@ -46,7 +50,30 @@ console.log("myCards", myCards);
 // Manipulation des cards
 //———————————————————————————————————————————————————————————
 
+//Trie des cards dans l'ordre
+//Pour éviter de voir le tableau trié avant, je fais un slice (car l'affectation est via reference et non par copie)
+console.log("Avant le tri : ", myCards.slice());
 
+let tmp;
+for(let i = 0; i < myCards.length-1; i++){
+  for(let j = i + 1; j < myCards.length; j++){
+    if(myCards[i].getValue() > myCards[j].getValue()){
+      tmp = myCards[i];
+      myCards[i] = myCards[j];
+      myCards[j] = tmp;
+    }
+  }
+}
+
+console.log("Après le tri : ", myCards);
+
+//L'ordi sort une carte au hasard parmis les 10
+const getRandomCard = () => {
+  return allCards[getRandom()];
+}
+
+console.log("getRandomCard()",getRandomCard());
 //-----------------------------------------------------------
 //Fin manipulation
 //-----------------------------------------------------------
+
